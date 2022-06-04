084cf6ec1aefd1e99d7c55aec4c3d4a44e494f31
Mariuskvaal <mariuskvaal1@gmail.com>
2022-06-04 23:48:54 +0200

    update

---
+++ js/script.js
@@ -0,0 +1,50 @@
+// question 1
+var product = {
+	name: "Chicken feet",
+	price: 149.99,
+};
+
+console.log(product.price)
+
+// question 2
+
+var animals = [
+
+	{
+		type: "cat",
+		colour: "black",
+	},
+	{
+		type: "elephant",
+		colour: "grey",
+	},
+	{
+		type: "dog",
+		colour: "brown",
+	},
+	{
+		type: "zebra",
+		colour: "black and white",
+	},
+];
+
+var animalsArray = [animals];
+
+for (var i = 0; i < animals.length; i++) {
+    console.log(animals[i].colour)
+}
+
+//question 3
+var dinnerOptions = [
+	{ dinner1: "Fish and Dips", price: 11.95, onSpecial: true },
+	{ dinner2: "Bacon and Frogs", price: 12.99, onSpecial: false },
+	{ dinner3: "Mince Flies", price: 19.0, onSpecial: true },
+];
+
+
+for (var i = 0; i < dinnerOptions.length; i++) {
+    
+}
+
+
+
